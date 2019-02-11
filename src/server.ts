c8e089a7530383866eb5138171bf60dc609b8e6e
Ben Garrison <ben.garrison@apiture.com>
2019-02-10 21:03:30 -0500

    updates

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
+// START HANDLERS
+/* Base Path Handler */
+app.get("/wilmjs", (req, res) => {
   res.send("You need API, Bro?!");
 });
 
-app.get("/meetup", (req, res) => {
+/* GET Meetups Handler */
+app.get("/wilmjs/meetups", (req, res) => {
   const meetups = fetchMeetups();
+  res.statusCode = 200;
   res.send(meetups);
 });
 
+/* GET Meetup Handler */
+app.get("/wilmjs/meetups/:meetupId", (req, res) => {
+  //TODO: implement
+});
+
+/* POST Meetup Handler */
+app.post("/wilmjs/meetups", (req, res) => {
+  //TODO: implement
+});
+// END HANDLERS
+
+/* Meetups Mock Service */
 function fetchMeetups(): Definitions.JsMeetups {
   const meetupsDefinition: Definitions.JsMeetups = {};
   const meetups: Definitions.JsMeetup[] = [];
@@ -23,8 +39,15 @@ function fetchMeetups(): Definitions.JsMeetups {
   return meetupsDefinition;
 }
 
-const meetup1: Definitions.JsMeetup = { name: "The Roots and the New" };
-const meetup2: Definitions.JsMeetup = { name: "SpookyJS" };
-const meetup3: Definitions.JsMeetup = { name: "Not Your Father's JavaScript" };
+/* Meetups Mock Data Store */
+const meetup1: Definitions.JsMeetup = {
+  id: "1",
+  name: "The Roots and the New"
+};
+const meetup2: Definitions.JsMeetup = { id: "2", name: "SpookyJS" };
+const meetup3: Definitions.JsMeetup = {
+  id: "3",
+  name: "Not Your Father's JavaScript"
+};
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
